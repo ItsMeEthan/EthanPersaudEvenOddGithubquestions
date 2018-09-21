5346dfe5c301091ad633217e9e6e8d660e42d5a2
BT_1N3_17@BKTECH.LOCAL <BT_1N3_17@BKTECH.LOCAL>
2018-09-21 08:45:18 -0400

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+    }
+        // write your code here
+        String input = "abcdefg";
+        System.out.println("input =" +input(input));
+        System.out.println("odd = " + odd(input));
+        System.out.println("even = " = even(input));
+        System.out.println("evenThenOdd = " + evenThenOdd(input));
+        if(input=5)
+            
+
